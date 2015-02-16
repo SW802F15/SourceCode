c89eae7cf2ad311fec684ba643749375e93615eb
cnduru11@student.aau.dk <cnduru11@student.aau.dk>
2015-02-16 10:33:47 +0100

    Set up test case

--- app/src/androidTest/java/com/example/sw802f15/tempoplayer/ApplicationTest.java
+++ app/src/androidTest/java/com/example/sw802f15/tempoplayer/ApplicationTest.java
@@ -2,12 +2,16 @@ package com.example.sw802f15.tempoplayer;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ TEST METHODS MUST BE PREFIXED WITH test....
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends InstrumentationTestCase
+{
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
     }
 }
