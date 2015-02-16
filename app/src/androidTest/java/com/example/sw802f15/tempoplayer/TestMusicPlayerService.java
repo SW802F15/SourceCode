33ca339cea0c82f8ab719f4b4ce2d7db80799dd3
Draegert <alexander_draegert@hotmail.com>
2015-02-16 13:13:17 +0100

    Tests for MusicPlayerService created.

--- app/src/androidTest/java/com/example/sw802f15/tempoplayer/TestMusicPlayerService.java
+++ app/src/androidTest/java/com/example/sw802f15/tempoplayer/TestMusicPlayerService.java
@@ -1,18 +1,34 @@
 package com.example.sw802f15.tempoplayer;
 
-import android.test.InstrumentationTestCase;
 import android.test.ServiceTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
 
+import junit.framework.Assert;
+
+//REMEMBER TO MOVE 'Test' TO END OF CLASS NAME.
 public class TestMusicPlayerService extends ServiceTestCase
 {
     /**
      * Constructor
      *
-     * @param serviceClass The type of the service under test.
      */
-    public TestMusicPlayerService(Class serviceClass) {
-        super(serviceClass);
+    public TestMusicPlayerService() {
+        super(MusicPlayerService.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        String pathToMp3File = "";
+    }
+
+    //Tests set up of test
+    @SmallTest
+    public void testPrecondition(){}
+
+
 
 
 }
