41556ccbe6223bfa34265a77bba36086dfc59eb2
Kristian <kthoms10@student.aau.dk>
2015-02-18 15:37:51 +0100

    added new files for settings

---
+++ app/src/androidTest/java/com/example/sw802f15/tempoplayer/SettingsActivityTest.java
@@ -0,0 +1,24 @@
+package com.example.sw802f15.tempoplayer;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+
+public class SettingsActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity> {
+
+    public SettingsActivityTest(){
+        super(SettingsActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception
+    {
+
+    }
+
+    @SmallTest
+    public void testStuff(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/sw802f15/tempoplayer/SettingsActivity.java
@@ -0,0 +1,8 @@
+package com.example.sw802f15.tempoplayer;
+
+import android.app.Activity;
+
+public class SettingsActivity extends Activity{
+
+
+}
