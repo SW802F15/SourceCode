4ad9fe0f852ac3f45f2ec24670f35be8c60b59b7
Draegert <alexander_draegert@hotmail.com>
2015-03-16 12:25:37 +0100

    Refactored and restructured MusicPlayerActivity.

--- app/src/main/java/dk/aau/sw802f15/tempoplayer/MusicPlayer/MusicPlayerActivity.java
+++ app/src/main/java/dk/aau/sw802f15/tempoplayer/MusicPlayer/MusicPlayerActivity.java
@@ -26,7 +26,6 @@ public class MusicPlayerActivity extends Activity{
 
     MusicPlayerService mService;
     boolean mBound = false;
-
     private Initializers _initializers;
 
     @Override
@@ -57,11 +56,6 @@ public class MusicPlayerActivity extends Activity{
     @Override
     protected void onStop() {
         super.onStop();
-        // Unbind from the service
-//        if (mBound) {
-//            unbindService(mConnection);
-//            mBound = false;
-//        }
     }
 
     @Override
@@ -83,10 +77,6 @@ public class MusicPlayerActivity extends Activity{
             unbindService(mConnection);
             mBound = false;
         }
-
-//        Intent musicPlayerService = new Intent(getApplicationContext(), MusicPlayerService.class);
-//        //musicPlayerService.setAction("Quit");
-//        stopService(musicPlayerService);
     }
 
     @Override
@@ -98,74 +88,13 @@ public class MusicPlayerActivity extends Activity{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == dk.aau.sw802f15.tempoplayer.R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent event)
-    {
-        int key = event.getKeyCode();
-
-        if(key == KeyEvent.KEYCODE_VOLUME_UP && event.getAction() == KeyEvent.ACTION_DOWN)
-        {
-            volumeUp();
-            return true;
-        }
-        else if(key == KeyEvent.KEYCODE_VOLUME_DOWN && event.getAction() == KeyEvent.ACTION_DOWN)
-        {
-            volumeDown();
-            return true;
-        }else
-        {
-            return super.dispatchKeyEvent(event);
-        }
-    }
-
-
-
-
-
-    public void volumeUp()
-    {
-        AudioManager am = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        final int curVol = am.getStreamVolume(AudioManager.STREAM_MUSIC);
-
-        if(curVol == am.getStreamMaxVolume(AudioManager.STREAM_MUSIC))
-        {
-            am.setStreamVolume(AudioManager.STREAM_MUSIC, curVol,
-                    AudioManager.FLAG_PLAY_SOUND+AudioManager.FLAG_SHOW_UI);
-        }else
-        {
-            am.setStreamVolume(AudioManager.STREAM_MUSIC, curVol + 1,
-                    AudioManager.FLAG_PLAY_SOUND+AudioManager.FLAG_SHOW_UI);
-        }
-    }
-
-    public void volumeDown()
-    {
-        AudioManager am = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        final int curVol = am.getStreamVolume(AudioManager.STREAM_MUSIC);
-
-        if(curVol == 0)
-        {
-            am.setStreamVolume(AudioManager.STREAM_MUSIC, curVol,
-                    AudioManager.FLAG_PLAY_SOUND+AudioManager.FLAG_SHOW_UI);
-        }else
-        {
-            am.setStreamVolume(AudioManager.STREAM_MUSIC, curVol - 1,
-                    AudioManager.FLAG_PLAY_SOUND+AudioManager.FLAG_SHOW_UI);
-        }
     }
 
     private ServiceConnection mConnection = new ServiceConnection() {
@@ -186,119 +115,57 @@ public class MusicPlayerActivity extends Activity{
         }
     };
 
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event)
+    {
+        int key = event.getKeyCode();
 
-    private void initializeTestSongs() {
-        //SongScanner.getInstance(this).findSongs();
-
-
-        /*
-        String initMusicPath = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_MUSIC + "/tempo/";
-        String initCoverPath = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_PICTURES + "/tempo/";
-
-        Song test_1 = new Song("Tristram", //Title
-                               "Matt Uemen", //Artist
-                               "Diablo SoundTrack", //Album
-                                113, //BPM
-                               Uri.parse(initMusicPath + "music_sample_1.mp3"), //SongUri
-                               Uri.parse(initCoverPath + "cover_sample_1.jpg"), //CoverUri
-                               7*60 + 40); //Duration
-
-        Song test_2 = new Song("Let It Go",
-                               "Idina Menzel",
-                               "Frozen SoundTrack",
-                               135,
-                               Uri.parse(initMusicPath + "music_sample_2.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_2.jpg"),
-                               3*60 + 40);
-
-        Song test_3 = new Song("Runnin'",
-                               "Adam Lambert",
-                               "Trespassing",
-                               81,
-                               Uri.parse(initMusicPath + "music_sample_3.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_3.jpg"),
-                               3*60 + 48);
-
-        Song test_4 = new Song("Sex on Fire",
-                               "Kings of Leon",
-                               "Only by the Night",
-                               153,
-                               Uri.parse(initMusicPath + "music_sample_4.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_4.jpg"),
-                               3*60 + 26);
-
-        Song test_5 = new Song("T.N.T.",
-                               "AC/DC",
-                               "T.N.T.",
-                               131,
-                               Uri.parse(initMusicPath + "music_sample_5.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_5.jpg"),
-                               3*60 + 34);
-
-        Song test_6 = new Song("Still Counting",
-                               "Volbeat",
-                               "Guitar Gangstars & Cadillac Blood",
-                               104,
-                               Uri.parse(initMusicPath + "music_sample_6.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_6.jpg"),
-                               4*60 + 21);
-
-        Song test_7 = new Song("Riverside",
-                               "Agnes Obel",
-                               "Philharmonics",
-                               100,
-                               Uri.parse(initMusicPath + "music_sample_7.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_7.jpg"),
-                               3*60 + 49);
-
-        Song test_8 = new Song("In The End",
-                               "Linkin Park",
-                               "",
-                               105,
-                               Uri.parse(initMusicPath + "music_sample_8.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_8.png"),
-                               3*60 + 36);
-
-        Song test_9 = new Song("Hurt",
-                               "Johnny Cash",
-                               "American IV: The Man Comes Around",
-                               98,
-                               Uri.parse(initMusicPath + "music_sample_9.mp3"),
-                               Uri.parse(initCoverPath + "cover_sample_9.jpg"),
-                               3*60 + 38);
-
-        Song test_10 = new Song("War of Change",
-                                "Thousand Foot Krutch",
-                                "The End Is Where We Begin",
-                                104,
-                                Uri.parse(initMusicPath + "music_sample_10.mp3"),
-                                Uri.parse(initCoverPath + "cover_sample_10.jpg"),
-                                3*60 + 51);
-
-
-        SongDatabase songDatabase = new SongDatabase(getApplicationContext());
-        songDatabase.clearDatabase();
-        songDatabase.insertSong(test_1);
-        songDatabase.insertSong(test_2);
-        songDatabase.insertSong(test_3);
-        songDatabase.insertSong(test_4);
-        songDatabase.insertSong(test_5);
-        songDatabase.insertSong(test_6);
-        songDatabase.insertSong(test_7);
-        songDatabase.insertSong(test_8);
-        songDatabase.insertSong(test_9);
-        songDatabase.insertSong(test_10);*/
+        if(key == KeyEvent.KEYCODE_VOLUME_UP && event.getAction() == KeyEvent.ACTION_DOWN) {
+            volumeUp();
+            return true;
+        }
+        else if(key == KeyEvent.KEYCODE_VOLUME_DOWN && event.getAction() == KeyEvent.ACTION_DOWN) {
+            volumeDown();
+            return true;
+        }
+        else {
+            return super.dispatchKeyEvent(event);
+        }
     }
 
+    public void volumeUp()
+    {
+        AudioManager audioManager = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        final int currentVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
+        if(currentVol == audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVol,
+                                         AudioManager.FLAG_PLAY_SOUND
+                                        +AudioManager.FLAG_SHOW_UI);
+        }
+        else {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVol + 1,
+                                         AudioManager.FLAG_PLAY_SOUND
+                                        +AudioManager.FLAG_SHOW_UI);
+        }
+    }
 
+    public void volumeDown()
+    {
+        AudioManager audioManager = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        final int currentVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
-
-
-
-
-
-
+        if(currentVol == 0) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVol,
+                                         AudioManager.FLAG_PLAY_SOUND
+                                        +AudioManager.FLAG_SHOW_UI);
+        }
+        else {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVol - 1,
+                                         AudioManager.FLAG_PLAY_SOUND
+                                        +AudioManager.FLAG_SHOW_UI);
+        }
+    }
 
     public void setBPMText(int bpm) {
         TextView BPMTextView = (TextView) findViewById(dk.aau.sw802f15.tempoplayer.R.id.textView_bpm);
@@ -314,46 +181,17 @@ public class MusicPlayerActivity extends Activity{
 
     public void setSongDurationText(int duration) {
         TextView songDurationTextView = (TextView) findViewById(dk.aau.sw802f15.tempoplayer.R.id.textView_songDuration);
-        Time time = new Time();
-        if (duration >= 3600) {
-            time.hour = duration / 3600;
-            duration = duration % 3600;
-        }
-        if (duration >= 60) {
-            time.minute = duration / 60;
-            duration = duration % 60;
-        }
-        time.second = duration;
 
-        String durationString = time.format("%M:%S");
+        SongTime songTime = new SongTime(duration);
 
-        if (time.hour > 0) {
-            durationString = time.format("%H:%M:%S");
-        }
-
-        songDurationTextView.setText(durationString);
+        songDurationTextView.setText(songTime.getFormattedSongTime());
     }
 
     public void setSongProgressText(int duration) {
         TextView songDurationTextView = (TextView) findViewById(dk.aau.sw802f15.tempoplayer.R.id.textView_currentPosition);
-        Time time = new Time();
-        if (duration >= 3600) {
-            time.hour = duration / 3600;
-            duration = duration % 3600;
-        }
-        if (duration >= 60) {
-            time.minute = duration / 60;
-            duration = duration % 60;
-        }
-        time.second = duration;
 
-        String durationString = time.format("%M:%S");
+        SongTime songTime = new SongTime(duration);
 
-        if (time.hour > 0) {
-            durationString = time.format("%H:%M:%S");
-        }
-
-        songDurationTextView.setText(durationString);
+        songDurationTextView.setText(songTime.getFormattedSongTime());
     }
-
 }
